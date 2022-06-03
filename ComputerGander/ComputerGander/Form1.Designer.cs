d23652be71d3d83d31656a3c07609380a2e951e3
jeads <60721703+jeads92@users.noreply.github.com>
2022-06-03 12:51:34 -0400

    Commit updated ignore file and project files

---
+++ ComputerGander/ComputerGander/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ComputerGander
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipAddressLabel = new System.Windows.Forms.Label();
+            this.ipButton = new System.Windows.Forms.Button();
+            this.macButton = new System.Windows.Forms.Button();
+            this.macLabel = new System.Windows.Forms.Label();
+            this.enterIPLabel = new System.Windows.Forms.Label();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.pingDataLabel = new System.Windows.Forms.Label();
+            this.generatePingData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ipAddressLabel
+            // 
+            this.ipAddressLabel.AutoSize = true;
+            this.ipAddressLabel.Location = new System.Drawing.Point(615, 398);
+            this.ipAddressLabel.Name = "ipAddressLabel";
+            this.ipAddressLabel.Size = new System.Drawing.Size(20, 15);
+            this.ipAddressLabel.TabIndex = 0;
+            this.ipAddressLabel.Text = "IP:";
+            // 
+            // ipButton
+            // 
+            this.ipButton.Location = new System.Drawing.Point(615, 353);
+            this.ipButton.Name = "ipButton";
+            this.ipButton.Size = new System.Drawing.Size(173, 23);
+            this.ipButton.TabIndex = 1;
+            this.ipButton.Text = "Show IP Address";
+            this.ipButton.UseVisualStyleBackColor = true;
+            this.ipButton.Click += new System.EventHandler(this.GenerateIP);
+            // 
+            // macButton
+            // 
+            this.macButton.Location = new System.Drawing.Point(331, 353);
+            this.macButton.Name = "macButton";
+            this.macButton.Size = new System.Drawing.Size(168, 23);
+            this.macButton.TabIndex = 2;
+            this.macButton.Text = "Show MAC Address";
+            this.macButton.UseVisualStyleBackColor = true;
+            this.macButton.Click += new System.EventHandler(this.GenerateMAC);
+            // 
+            // macLabel
+            // 
+            this.macLabel.AutoSize = true;
+            this.macLabel.Location = new System.Drawing.Point(331, 398);
+            this.macLabel.Name = "macLabel";
+            this.macLabel.Size = new System.Drawing.Size(37, 15);
+            this.macLabel.TabIndex = 3;
+            this.macLabel.Text = "MAC:";
+            // 
+            // enterIPLabel
+            // 
+            this.enterIPLabel.AutoSize = true;
+            this.enterIPLabel.Location = new System.Drawing.Point(6, 266);
+            this.enterIPLabel.Name = "enterIPLabel";
+            this.enterIPLabel.Size = new System.Drawing.Size(50, 15);
+            this.enterIPLabel.TabIndex = 5;
+            this.enterIPLabel.Text = "Enter IP:";
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(77, 258);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(199, 23);
+            this.ipTextBox.TabIndex = 6;
+            // 
+            // pingDataLabel
+            // 
+            this.pingDataLabel.AutoSize = true;
+            this.pingDataLabel.Location = new System.Drawing.Point(15, 357);
+            this.pingDataLabel.Name = "pingDataLabel";
+            this.pingDataLabel.Size = new System.Drawing.Size(0, 15);
+            this.pingDataLabel.TabIndex = 8;
+            // 
+            // generatePingData
+            // 
+            this.generatePingData.Location = new System.Drawing.Point(77, 316);
+            this.generatePingData.Name = "generatePingData";
+            this.generatePingData.Size = new System.Drawing.Size(197, 23);
+            this.generatePingData.TabIndex = 9;
+            this.generatePingData.Text = "Generate Ping Data";
+            this.generatePingData.UseVisualStyleBackColor = true;
+            this.generatePingData.Click += new System.EventHandler(this.GeneratePing);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Example: 12.345.678.90";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.generatePingData);
+            this.Controls.Add(this.pingDataLabel);
+            this.Controls.Add(this.ipTextBox);
+            this.Controls.Add(this.enterIPLabel);
+            this.Controls.Add(this.macLabel);
+            this.Controls.Add(this.macButton);
+            this.Controls.Add(this.ipButton);
+            this.Controls.Add(this.ipAddressLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label ipAddressLabel;
+        private Button ipButton;
+        private Button macButton;
+        private Label macLabel;
+        private Label enterIPLabel;
+        private TextBox ipTextBox;
+        private Label pingDataLabel;
+        private Button generatePingData;
+        private Label label1;
+    }
+}
---
+++ ComputerGander/ComputerGander/Form1.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ComputerGander
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateMAC(object sender, EventArgs e)
+        {
+            // Find and display the MAC Address of the host machine.
+
+            if (macButton.Text == "Show MAC Address")
+            {
+                // Get MAC address from Network Interfaces
+                var macAddr =
+                (
+                    from nic in NetworkInterface.GetAllNetworkInterfaces()
+                    where nic.OperationalStatus == OperationalStatus.Up
+                    select nic.GetPhysicalAddress().ToString()
+                ).FirstOrDefault();
+
+                // Update the button and text labels
+
+                macLabel.Text = $"MAC: {macAddr}";
+                macButton.Text = "Hide MAC Address";
+            }
+
+            // Hide the MAC address
+
+            else
+            {
+                macLabel.Text = "MAC Address Hidden";
+                macButton.Text = "Show MAC Address";
+            }
+
+        }
+
+        private void GenerateIP(object sender, EventArgs e)
+        {
+            // Find and display the IP Address of the host machine.
+
+            if(ipButton.Text == "Show IP Address")
+            {
+                string hostName = Dns.GetHostName();
+                string localIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+                ipAddressLabel.Text = $"IP: {localIP}";
+                ipButton.Text = "Hide IP Address";
+            }
+
+            // Hide the IP Address
+            else
+            {
+                ipButton.Text = "Show IP Address";
+                ipAddressLabel.Text = "IP address hidden";
+            }
+            
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GeneratePing(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] args = { ipTextBox.Text };
+                Ping pingSender = new Ping();
+                PingOptions options = new PingOptions();
+
+                // Use the default Ttl value which is 128,
+                // but change the fragmentation behavior.
+                options.DontFragment = true;
+
+                // Create a buffer of 32 bytes of data to be transmitted.
+                string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 120;
+                PingReply reply = pingSender.Send(args[0], timeout, buffer, options);
+                if (reply.Status == IPStatus.Success)
+                {
+                    pingDataLabel.Text = $"Address: {reply.Address.ToString()}\n" +
+                        $"RoundTrip time: {reply.RoundtripTime}\n" +
+                        $"Time to Live: {reply.Options.Ttl}\n" +
+                        $"Buffer size: {reply.Buffer.Length}";
+                }
+            }
+            catch (PingException)
+            {
+                pingDataLabel.Text = "Not a valid IP";
+            }
+            catch (ArgumentNullException)
+            {
+                pingDataLabel.Text = "Cannot enter null or empty value";
+            }
+            catch(Exception)
+            {
+                pingDataLabel.Text = "Not a valid IP";
+            }
+        }
+    }
+}
